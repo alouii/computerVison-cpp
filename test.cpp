4a40f86a3834a3ee92dba7364da1b89f10fe01f8
alouii <aloui.lassaad@gmail.com>
2026-01-04 14:25:55 +0100

    commit (#0004)

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int main(){
     vector <int>v1(10,0);
-    cout << "Sizeof vector  V:" v1.size();++i);
+    cout << "Sizeof vectorV:"<< v1.size() << endl;
     for(unsigned i=0;i<v1.size();++i){
         cout << v1[i] << " ";
     }
