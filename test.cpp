c2290f3427bd803910af677c2aabd1dedab4f053
alouii <aloui.lassaad@gmail.com>
2026-01-04 14:16:37 +0100

    commit (#0002)

--- test.cpp
+++ test.cpp
@@ -1,10 +1,8 @@
+class A;
+vector <int> v1;
+vector <float> v2;
+vector <A> v3;
 int main(){
-    vector<A*> v1;
-    v1.push_back(new A(1));
-    cout<<"First ready!\n";
-    v1.push_back(new A(2));
-    cout<<"Secondary ready!\n";
-    v1.push_back(new A(3));
-    cout<<"Tertiary ready!\n";
+    ;
     return 0;       
 }
